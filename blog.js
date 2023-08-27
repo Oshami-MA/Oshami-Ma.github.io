0da6dc3776b4d68600dbfb20a3e76c674681d393
Emma <emma.gueffier1@gmail.com>
2023-08-27 11:20:10 +0900

    First commit

---
+++ blog.js
@@ -0,0 +1,50 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "400px";
+    document.getElementById("main").style.marginLeft = "400px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.body.style.backgroundColor = "white";
+}
+
+
+
+images = document.getElementsByClassName('img');
+counter = 0;
+
+setInterval(myTimer, 2000);
+
+function myTimer() {
+    console.log (counter);
+	    document.getElementsByClassName('show')[0].classList.remove('show');
+	    images[counter].classList.add('show');
+    if(counter < images.length - 1) {
+        counter = counter + 1;
+    } else {
+        counter = 0;
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h4');
+
+
+
+function setUserName(){
+    let myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Welcome, ' + myName;
+}
+
+if (!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Welcome, ' + storedName;
+}
+myButton.addEventListener('click', function() {
+    setUserName();
+});
