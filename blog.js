7e35e68b4020273aedd331be8d2eb4362ea5793f
Emma <emma.gueffier1@gmail.com>
2023-08-28 21:54:32 +0900

    redesign of website e Please enter the commit message for your changes. Lines starting

--- blog.js
+++ blog.js
@@ -1,16 +1,30 @@
+var isNavOpen = false;
+
 function openNav() {
     document.getElementById("mySidenav").style.width = "400px";
-    document.getElementById("main").style.marginLeft = "400px";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    isNavOpen = true;
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
     document.body.style.backgroundColor = "white";
+    isNavOpen = false;
 }
 
-
+window.addEventListener('click', function (e) {
+    var target = e.target;
+    if (e.target.id === 'collapse') {
+        if (isNavOpen) {
+            closeNav()
+        } else {
+            openNav()
+        }
+    }
+    else if (!target.closest('#mySidenav')) {
+        closeNav()
+    }
+})
 
 images = document.getElementsByClassName('img');
 counter = 0;
@@ -18,10 +32,10 @@ counter = 0;
 setInterval(myTimer, 2000);
 
 function myTimer() {
-    console.log (counter);
-	    document.getElementsByClassName('show')[0].classList.remove('show');
-	    images[counter].classList.add('show');
-    if(counter < images.length - 1) {
+    console.log(counter);
+    document.getElementsByClassName('show')[0].classList.remove('show');
+    images[counter].classList.add('show');
+    if (counter < images.length - 1) {
         counter = counter + 1;
     } else {
         counter = 0;
@@ -33,7 +47,7 @@ let myHeading = document.querySelector('h4');
 
 
 
-function setUserName(){
+function setUserName() {
     let myName = prompt('Please enter your name.');
     localStorage.setItem('name', myName);
     myHeading.textContent = 'Welcome, ' + myName;
@@ -45,6 +59,6 @@ if (!localStorage.getItem('name')) {
     let storedName = localStorage.getItem('name');
     myHeading.textContent = 'Welcome, ' + storedName;
 }
-myButton.addEventListener('click', function() {
+myButton.addEventListener('click', function () {
     setUserName();
 });
